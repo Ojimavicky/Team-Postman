648f0a1a4f7139e7ec19ed5a2937c4fab509ddce
Ojimavicky <Ojimavictoria3@gmail.com>
2023-07-21 11:44:14 -0700

    KongaWebTest

---
+++ KongaWebTest/src/main/java/KongaWebTest.java
@@ -0,0 +1,29 @@
+public class KongaWebTest {
+
+    /*
+    open chrome browser
+    maximise the browser
+
+  input the URL Konga-
+  click on sign in button
+  enter first name
+  enter last name
+  phone number
+  enter password
+  click create account
+  input otp code
+     -From the Categories, click on the “Computers and Accessories”
+     -Click on the Laptop SubCategory
+    -Click on the Apple MacBooks-Add an item to the cart-
+    click on add to cart icon
+
+    Select Address-
+    Continue to make payment-
+    Select a Card Payment Method-
+    Input invalid card details-
+    Print Out the error message: Invalid card number
+    -Close the iFrame that displays the input card Modal
+    -Quit the browse*//
+
+
+}
